213580f35fdd0430becb65b2e81ddc6aca5345c6
Mariakj <36694059+Mariakj@users.noreply.github.com>
2025-11-21 15:18:39 +0530

    Delete employee directory

--- employee/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from employee.models import Employee, LeaveRequest
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(LeaveRequest)
