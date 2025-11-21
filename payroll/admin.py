5d61c1ecd1a670015b12f84a07ceb8cb7c749234
Mariakj <36694059+Mariakj@users.noreply.github.com>
2025-11-21 15:18:54 +0530

    Delete payroll directory

--- payroll/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from payroll.models import Payroll, Payslip, TaxDeduction
-
-# Register your models here.
-admin.site.register(Payroll)
-admin.site.register(Payslip)
-admin.site.register(TaxDeduction)
-
